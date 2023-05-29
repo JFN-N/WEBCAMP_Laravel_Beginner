14ba5444d6e2afd1de463284382dcaa860a8db41
SATO <sato@example.com>
2023-05-29 13:37:12 +0000

    NOW4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,8 @@ use App\Http\Requests\LoginPostRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
+use App\Models\User as UserModel;
+
 use App\Http\Requests\UserRegisterPostRequest;
 
 class UserController extends Controller
@@ -15,45 +17,50 @@ class UserController extends Controller
     {
         return view('user.register');
     }
-    /*
+
     public function register(UserRegisterPostRequest $request)
     {
         // validate済みのデータの取得
         $datum = $request->validated();
-        var_dump($datum); exit;
+
+        // user_id の追加
+        $datum['user_id'] = Auth::id();
 
         // テーブルへのINSERT
-        //try {
-        //    $r = TaskModel::create($datum);
-//var_dump($r); exit;
-//        } catch(\Throwable $e) {
-//            // XXX 本当はログに書く等の処理をする。今回は一端「出力する」だけ
-//            echo $e->getMessage();
-//           exit;
-
-        // タスク登録成功
-        //$request->session()->flash('front.user_register_success', true);
-
-        //
-        //return redirect('/login');
-    }
-    */
-
-
-
-    public function register(UserRegisterPostRequest $request)
-    {
-        $this -> validator($request->all())->validate();
-        event(new Registered($user = $this->create($request->all())));
-        $this->guard()->login($user);
-        return $this->registered($request, $user)
-                           ?: refirect($this->redirectPath());
+        try {
+            $r = UserModel::create($datum);
+            var_dump($r); exit;
+        } catch(\Throwable $e) {
+            // XXX 本当はログに書く等の処理をする。今回は一端「出力する」だけ
+            echo $e->getMessage();
+            exit;
+        }
 
         // タスク登録成功
         $request->session()->flash('front.user_register_success', true);
 
 
         return redirect('/login');
+
+        /*
+
+        // validate済みのデータの取得
+        $datum = $request->validated();
+
+        // user_id の追加
+        $datum['user_id'] = Auth::id();
+
+        // テーブルへのINSERT
+        try {
+            $r = UserModel::create($datum);
+var_dump($r); exit;
+        } catch(\Throwable $e) {
+            // XXX 本当はログに書く等の処理をする。今回は一端「出力する」だけ
+            echo $e->getMessage();
+            exit;
+        }
+
+        */
     }
 
 }
